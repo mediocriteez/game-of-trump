9d6c17a28892e686f546e7c617e612b74a16a735
Jacob <jacobwintr@gmail.com>
2024-05-16 22:07:18 -0400

    hopefully provide fullscreen iphone

--- src/app/game/lambo/page.js
+++ src/app/game/lambo/page.js
@@ -26,10 +26,10 @@ const Lambo = () => {
 
     return(
         <>
-            <div onClick={enterFullscreen} style={{placeItems: 'center', background:'black', color: 'white', display: buttonShow ? 'grid' : 'none', position: 'fixed', zIndex: 2, inset: 0, fontSize: '2rem', padding: '7px 12px', fontFamily:'Arcade Classic', border: 'none', outline: 'none', textAlign: 'center'}}>
+            {/* <div onClick={enterFullscreen} style={{placeItems: 'center', background:'black', color: 'white', display: buttonShow ? 'grid' : 'none', position: 'fixed', zIndex: 2, inset: 0, fontSize: '2rem', padding: '7px 12px', fontFamily:'Arcade Classic', border: 'none', outline: 'none', textAlign: 'center'}}>
                 <div>Click / Tap<br /> to Start</div>
-            </div>
-            <iframe ref={iframeRef} src={'https://663edfd8e261e58d2476a8a3--glistening-tartufo-5af5f5.netlify.app/'} style={{position: 'fixed', zIndex: 1, width:'100vw', height: '100vh'}}></iframe>
+            </div> */}
+            <iframe ref={iframeRef} src={'https://663edfd8e261e58d2476a8a3--glistening-tartufo-5af5f5.netlify.app/'} style={{position: 'fixed', zIndex: 1, inset: 0, width: '100vw', height: '100vh'}}></iframe>
         </>
     )
 }
---
+++ src/app/game/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+    "apple-mobile-web-app-capable": "yes",
+    "apple-mobile-web-app-status-bar-style": "black-translucent"
+}
+
+const layout = ({children}) => {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default layout
