3e572eb5b50058b90dc5dd1ba9fe983c2ef55dd8
Jacob <jacobwintr@gmail.com>
2024-05-16 22:16:18 -0400

    apple fullscreen maybe?

--- src/app/game/layout.js
+++ src/app/game/layout.js
@@ -1,4 +1,8 @@
 export const metadata = {
+    appleWebApp: {
+        capable: 'yes',
+        statusBarStyle: 'black-translucent'
+    },
     "apple-mobile-web-app-capable": "yes",
     "apple-mobile-web-app-status-bar-style": "black-translucent"
 }
