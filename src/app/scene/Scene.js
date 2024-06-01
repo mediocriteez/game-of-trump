7f17d734c3c40158a41b5b9c0dfbda6ded1e6d52
Jacob <jacobwintr@gmail.com>
2024-05-31 20:26:55 -0400

    fix ios autoplay issue

--- src/app/scene/Scene.js
+++ src/app/scene/Scene.js
@@ -59,7 +59,7 @@ const Scene = ({setAAction, setCurrentContent}) => {
             <div className={css.titleCard} data-fade-out={titleFade}>
                 <span>Washington D.C. , 2019</span>
             </div>
-            <video ref={sceneVideoRef} playsinline className={css.sceneVideo} src={'/scene.mp4'}></video>
+            <video ref={sceneVideoRef} autoplay playsinline className={css.sceneVideo} src={'/scene.mp4'}></video>
             <div onClick={aAction} className={css.buttonDescription} style={{fontSize: '1.5rem', cursor: 'pointer'}}><span style={{display:'inline-grid', placeItems: 'center', width: '30px', aspectRatio: '1/1', borderRadius: '50%', background: 'white', color: 'black', marginRight: '5px'}}>A</span> skip</div>
         </div>
     )
